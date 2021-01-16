e9bc74c0124c8777e5146877175cc8e0fa3a7303
sameer1406 <sameermohd1406@gmail.com>
2021-01-16 19:09:26 +0530

    updated the youtube library

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, Response, make_response
+from flask import Flask, request, jsonify
 from  pytube import YouTube
 
 app = Flask(__name__)
@@ -7,18 +7,18 @@ app = Flask(__name__)
 def youtube():
     youtube_link_request = request.get_json(force=False, silent=False, cache=True)
     youtube_link = youtube_link_request['link']
-    yt = YouTube(youtube_link)
-    views = yt.views
-    thumbnail = yt.thumbnail_url
-    title = yt.title
-    streams_data = yt.streams
-    streams = []
-    for stream in streams_data:
-        stream_info = stream
-        streams.append(stream_info.type)
+    yt = YouTube(youtube_link).streams.first().download()
+    print(youtube_link)
+    views = YouTube(youtube_link).views
+    thumbnail = YouTube(youtube_link).thumbnail_url
+    title = YouTube(youtube_link).title
+    # streams_data = yt.streams.first().download()
+    # streams = []
+    # for stream in streams_data:
+    #     stream_info = stream
+    #     streams.append(stream_info.type)
     return jsonify({"Data": "successful completed",
-                    "info": streams,
-                    "views": views,
+                     "views": views,
                     "thumbnail":thumbnail,
                     "title" :title
                     }), 200
