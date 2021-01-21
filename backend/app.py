0da643f98e08404642c46189f8018baa1d4b81a3
sameer1406 <sameermohd1406@gmail.com>
2021-01-21 23:26:14 +0530

    first push to heroku

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,24 @@
 from flask import Flask, request, jsonify
-from  pytube import YouTube
+from pytube import YouTube
+import os
+from pathlib import Path
 
 app = Flask(__name__)
+
 
 @app.route('/Youtube', methods=["POST"])
 def youtube():
     youtube_link_request = request.get_json(force=False, silent=False, cache=True)
+    print(youtube_link_request)
     youtube_link = youtube_link_request['link']
-    yt = YouTube(youtube_link).streams.first().download()
-    print(youtube_link)
+    # downloads = youtube_link_request['path']
+    # print(downloads);
+    path = "/Users/sameer/Documents/YouTube_mobile/YouTUBE_APP/backend/downloads"
+    yt = YouTube(youtube_link).streams.first().download(path)
+    # files =
+    for file in os.listdir(path):
+        print(file)
+    # print(youtube_link)
     views = YouTube(youtube_link).views
     thumbnail = YouTube(youtube_link).thumbnail_url
     title = YouTube(youtube_link).title
@@ -18,9 +28,10 @@ def youtube():
     #     stream_info = stream
     #     streams.append(stream_info.type)
     return jsonify({"Data": "successful completed",
-                     "views": views,
-                    "thumbnail":thumbnail,
-                    "title" :title
+                    "views": views,
+                    "thumbnail": thumbnail,
+                    "title": title,
+
                     }), 200
 
 
